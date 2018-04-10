e29d0c23b684a21e9aa280df1022b8ca289bf00e
lum1nary <skaletsky.serhiy@gmail.com>
2018-04-10 08:26:05 +0300

    merged rules to validation service

--- decorator-validation-example/DecoratorValidation.Core/Validation/IValidationService.cs
+++ decorator-validation-example/DecoratorValidation.Core/Validation/IValidationService.cs
@@ -1,10 +1,14 @@
-﻿namespace DecoratorValidation.Core
+﻿using System.Collections.Generic;
+
+namespace DecoratorValidation.Core
 {
     public interface IValidationService
     {
+        IEnumerable<IValidationRule> GetRules(NodeType nodeType);
+
+        void RegisterRules(NodeType nodeType, params IValidationRule[] rules);
+
         void Register(IValidationNode node);
         void Unregister(IValidationNode node);
-
-        void QueryValidation(IValidationNode root);
     }
 }
--- decorator-validation-example/DecoratorValidation.Core/Validation/ValidationService.cs
+++ decorator-validation-example/DecoratorValidation.Core/Validation/ValidationService.cs
@@ -1,11 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace DecoratorValidation.Core
 {
     public class ValidationService : IValidationService
     {
+        private readonly IDictionary<NodeType, IList<IValidationRule>> _nodeRules = new Dictionary<NodeType, IList<IValidationRule>>();
+
         private readonly HashSet<IValidationNode> _nodes = new HashSet<IValidationNode>();
+
+        public IEnumerable<IValidationRule> GetRules(NodeType nodeType)
+        {
+            return _nodeRules.TryGetValue(nodeType, out var rules) ? 
+                rules : 
+                new IValidationRule[0];
+        }
+
+        public void RegisterRules(NodeType nodeType, params IValidationRule[] rules)
+        {
+            IList<IValidationRule> existingRules;
+            if (_nodeRules.TryGetValue(nodeType, out existingRules))
+            {
+                foreach (var rule in rules)
+                {
+                    existingRules.Add(rule);
+                }
+
+                return;
+            }
+
+            existingRules = new List<IValidationRule>(rules);
+            _nodeRules.Add(nodeType, existingRules);
+        }
 
         public void Register(IValidationNode node)
         {
@@ -15,11 +40,6 @@ namespace DecoratorValidation.Core
         public void Unregister(IValidationNode node)
         {
             _nodes.Remove(node);
-        }
-
-        public void QueryValidation(IValidationNode root)
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
